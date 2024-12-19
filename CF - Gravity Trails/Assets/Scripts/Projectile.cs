1470068473252ff5b1b738b4a856324dde20afc8
Bernice <markhamonca@codeninjas.com>
2024-12-18 19:48:56 -0500

    Step 114 of Gravity Trails

--- CF - Gravity Trails/Assets/Scripts/Projectile.cs
+++ CF - Gravity Trails/Assets/Scripts/Projectile.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public Throwable direction;
+    public float speed;
+    public Teleport teleport;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = GameObject.FindGameObjectWithTag("Player").GetComponent<Throwable>();
+        teleport = GameObject.FindGameObjectWithTag("Exit").GetComponent<Teleport>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += direction.offset * Time.deltaTime * speed;
+        Invoke("destroyThrowable", 5);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            teleport.enemyCount -= 1;
+        }
+    }
+
+    private void destroyThrowable()
+    {
+        Destroy(gameObject);
     }
 }
--- CF - Gravity Trails/Assets/Scripts/Throwable.cs
+++ CF - Gravity Trails/Assets/Scripts/Throwable.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Throwable : MonoBehaviour
 {
 
     public GameObject objectThrown;
     public Vector3 offset;
     public int throwableCounter;
-
+    public Text collectableCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -22,18 +22,20 @@ public class Throwable : MonoBehaviour
         if (Input.GetButtonDown("Fire1") && throwableCounter >= 1)
         {
             Debug.Log("fire");
+            offset = transform.localScale.x * new Vector3(1, 0, 0);
             Vector3 throwablePosition = transform.position + offset;
             Instantiate(objectThrown, throwablePosition, transform.rotation);
-            offset = transform.localScale.x * new Vector3(1, 0, 0);
             throwableCounter -= 1;
+            collectableCounter.text = throwableCounter.ToString();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == ("Collectables"))
+        if (collision.gameObject.tag == ("Collectable"))
         {
             throwableCounter += 1;
+            collectableCounter.text = throwableCounter.ToString();
             Destroy(collision.gameObject);
         }
     }
