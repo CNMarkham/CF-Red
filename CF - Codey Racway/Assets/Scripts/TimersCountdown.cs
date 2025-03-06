70af2b622d3faa3f8d1bdac64050b9c34af26a66
Who <markhamonca@codeninjas.com>
2025-03-05 19:48:32 -0500

    Step 108 of Codey Raceway

---
+++ CF - Codey Racway/Assets/Scripts/TimersCountdown.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimersCountdown : MonoBehaviour
+{
+    public Text lapTime;
+    public Text startCountdown;
+
+    public Text totalLapTime;
+    public float totalCountdownTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
